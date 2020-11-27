ced09a16c2bafc0f6cd68a73c8faed3b1f5049c9
liuheng <cooper.liuheng@outlook.com>
2020-11-27 20:18:20 +0800

    test1

--- src/com/liuheng/hello_world.java
+++ src/com/liuheng/hello_world.java
@@ -1,6 +1,6 @@
 package com.liuheng;
 public class hello_world {
     public static void main(String[] args) {
-        System.out.println("Hi,World!I am liuheng");
+        System.out.println("Hi,World!I am liuheng,hope I could do things I want to do");
     }
 }
