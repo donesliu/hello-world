3ddeb36419bee803a4452fb7587a5e712d95eed7
liuheng <cooper.liuheng@outlook.com>
2020-11-27 20:24:43 +0800

    test2

--- src/com/liuheng/hello_world.java
+++ src/com/liuheng/hello_world.java
@@ -1,6 +1,6 @@
 package com.liuheng;
 public class hello_world {
     public static void main(String[] args) {
-        System.out.println("Hi,World!I am liuheng,hope I could do things I want to do");
+        System.out.println("Hi,World!I am liuheng,hope I could do things I want to do,I have a dream,want to come back children time");
     }
 }
